f3f3ead8abf4044154acfcf1bd1950a24ff89bc1
foti <footios76@gmail.com>
2022-08-17 17:41:00 +0300

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'testing-library'],
+  rules: {
+    'react/prop-types': ['error', { ignore: ['navigation'] }],
+    '@typescript-eslint/no-var-requires': 0,
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off',
+    'testing-library/consistent-data-testid': [
+      2,
+      {
+        testIdPattern: '^TestId(__[A-Z]*)?$',
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
+};
---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TabMe } from './src/components';
+
+const App = () => {
+  return <TabMe />;
+};
+
+export default App;
